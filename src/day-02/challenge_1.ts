0ab2c2a2d7e51897531600c399889c1895aeb15c
Mika Hiltunen <mika.hiltunen@smartly.io>
2021-12-02 07:39:25 +0200

    Day 2

---
+++ src/day-02/challenge_1.ts
@@ -0,0 +1,26 @@
+import { readInput } from "../helpers/input";
+
+const inputFile = `${__dirname}/input.txt`;
+
+const solve = () => {
+    const instructions = readInput(inputFile);
+    let horizontal = 0;
+    let depth = 0;
+    for (const instruction of instructions) {
+        const [ direction, steps ] = instruction.split(" ");
+        switch (direction) {
+            case "forward":
+                horizontal += parseInt(steps);
+                break;
+            case "down":
+                depth += parseInt(steps);
+                break;
+            case "up":
+                depth -= parseInt(steps);
+                break;
+        }
+    }
+    return horizontal * depth;
+}
+
+export default solve;
