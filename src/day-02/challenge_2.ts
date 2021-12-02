3fcf8bfff8ca7fdc62accb9b233b9f019ee871de
Mika Hiltunen <saaste@users.noreply.github.com>
2021-12-02 07:40:34 +0200

    Day 02 (#1)
    * Day 2
    
    * Unit tests

---
+++ src/day-02/challenge_2.ts
@@ -0,0 +1,29 @@
+import { readInput } from "../helpers/input";
+
+const inputFile = `${__dirname}/input.txt`;
+
+export const solve = () => {
+    const instructions = readInput(inputFile);
+    let horizontal = 0;
+    let depth = 0;
+    let aim = 0;
+    for (const instruction of instructions) {
+        const [ direction, rawSteps ] = instruction.split(" ");
+        const steps = parseInt(rawSteps);
+        switch (direction) {
+            case "forward":
+                horizontal += steps;
+                depth += aim * steps;
+                break;
+            case "down":
+                aim += steps;
+                break;
+            case "up":
+                aim -= steps;
+                break;
+        }
+    }
+    return horizontal * depth;
+}
+
+export default solve;
